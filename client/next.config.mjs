69af6fd600ccc9898e2ac24be0089f2c65d2b7e3
Emhemed Bin Said <msaed77@gmail.com>
2025-10-05 12:22:01 +0200

    server port 5000

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,11 +4,11 @@ const nextConfig = {
     return [
       {
         source: '/api/auth/:path*',
-        destination: 'http://localhost:8000/api/auth/:path*',
+        destination: 'http://localhost:5000/api/auth/:path*',
       },
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8000/api/:path*',
+        destination: 'http://localhost:5000/api/:path*',
       },
     ];
   },
